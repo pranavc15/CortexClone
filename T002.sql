54b84499f9c30f2e6b0a19c2e0b4b812f982bd24
pranavc15 <142785502+pranavc15@users.noreply.github.com>
2023-08-22 19:34:35 +0200

    Create T002.sql

---
+++ T002.sql
@@ -0,0 +1,6 @@
+SELECT
+    SPRAS AS LanguageKey_SPRAS,
+    LASPEZ AS LanguageSpecifications_LASPEZ,
+    LAHQ AS DegreeOfTranslationOfLanguage_LAHQ,
+    LAISO AS TwoCharacterSapLanguageCode_LAISO
+FROM T002
